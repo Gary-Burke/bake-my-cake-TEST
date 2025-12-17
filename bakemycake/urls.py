c470399dd08b5f369d9bf94c38aaac94ff302913
gary-burke <garylewisburke@gmail.com>
2025-12-17 20:43:12 +0100

    Setup Django AllAuth for users

--- bakemycake/urls.py
+++ bakemycake/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path("", include("pages.urls"), name="pages-urls"),
     path("", include("cakebuilder.urls"), name="cakebuilder-urls"),
